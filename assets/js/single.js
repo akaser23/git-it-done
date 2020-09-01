9af0383b85ecc8c0740376571d7d6331f0aa20f4
Anna Kaser <kaserann96@gmail.com>
2020-08-31 20:32:18 -0600

    error handling and page redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,26 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,15 +30,15 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
-            });
-        }
-        else {
-            alert("There was a problem with your request!");
-        }
 
-        // check if api has paginated issues
-        if (response.headers.get("Link")) {
-            displayWarning(repo);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -72,4 +93,5 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
